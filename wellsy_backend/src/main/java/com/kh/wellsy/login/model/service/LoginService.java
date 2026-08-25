49ffb8cd9e72d3b2961956974ec086c4730fd6b4
Jia-Lim11 <jihyelim331@gmail.com>
2026-08-25 10:25:13 +0900

    주석 추가
    헷갈리지 않도록 단계별 주석 추가

--- wellsy_backend/src/main/java/com/kh/wellsy/login/model/service/LoginService.java
+++ wellsy_backend/src/main/java/com/kh/wellsy/login/model/service/LoginService.java
@@ -5,5 +5,7 @@ import com.kh.wellsy.employee.model.vo.Employee;
 
 public interface LoginService {
 
+	// 아이디로 재직중인 직원을 찾아서 반환
+	// 없으면 Optional.empty()가 반환됨 (null 대신 안전하게 처리)
 	Optional<Employee> loginEmploy(String loginId);
 }
--- wellsy_backend/src/main/java/com/kh/wellsy/login/model/service/LoginServiceImpl.java
+++ wellsy_backend/src/main/java/com/kh/wellsy/login/model/service/LoginServiceImpl.java
@@ -9,11 +9,13 @@ import com.kh.wellsy.login.model.dao.EmployeeRepository;
 @Service
 public class LoginServiceImpl implements LoginService {
 
+	// employee Repository 그대로 사용
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
 	public Optional<Employee> loginEmploy(String loginId) {
 		
 		return employeeRepository.findByLoginIdAndStatus(loginId, "EMPLOYED"); // "Y"
+		// STATUS가 EMPLOYED(재직중)인 사람 중에 이 loginId를 가진 사람을 조회
 	}
 }
