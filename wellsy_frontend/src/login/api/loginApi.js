79fd73ea6f895989b9e781fcb566ec1ebf5e50f3
Jia-Lim11 <jihyelim331@gmail.com>
2026-08-25 14:46:54 +0900

    프론트엔드- loginApi.js
    프론트엔드- loginApi.js 구현

---
+++ wellsy_frontend/src/login/api/loginApi.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8006/wellsy";
+
+// 로그인 함수 요청
+// employee 객체 { loginId, password }를 받아서 백엔드에 POST 요청
+const loginEmployeeApi = (employee) => {
+    
+    return axios({
+        url : `${BASE_URL}/login`,
+        method : "post",
+        data : employee
+    });
+};
+
+// 내보내기
+export { loginEmployeeApi };
