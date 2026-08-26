fb379d2ee59062b50ecb6e656487a8a33e2affdc
papo1022 <85226430+papo1022@users.noreply.github.com>
2026-08-26 17:45:26 +0900

    Update EmployeeDao.java

--- wellsy_backend/src/main/java/com/kh/wellsy/employee/model/dao/EmployeeDao.java
+++ wellsy_backend/src/main/java/com/kh/wellsy/employee/model/dao/EmployeeDao.java
@@ -1,6 +1,7 @@
 package com.kh.wellsy.employee.model.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
