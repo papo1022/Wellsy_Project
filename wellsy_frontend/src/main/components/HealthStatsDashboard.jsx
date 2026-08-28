2a3408d5f17169930cac4ad225883da098c334f8
Jia-Lim11 <jihyelim331@gmail.com>
2026-08-28 12:08:48 +0900

    .

--- wellsy_frontend/src/main/components/HealthStatsDashboard.jsx
+++ wellsy_frontend/src/main/components/HealthStatsDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/HealthStatesDashboard.css";
+import "../styles/HealthStatsDashboard.css";
 
 
 const API_URL =
