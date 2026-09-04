4f44d4f78076f5c0cc1e8f6205afe43b7a22587c
Jia-Lim11 <jihyelim331@gmail.com>
2026-09-04 14:34:45 +0900

    AI 챗봇 컨트롤러 구현
    프론트(aiApi.js)에서 이미 POST /wellsy/ai/chat으로 요청을 보내게 만들어뒀으므로 그 경로에 맞춰서 구현

--- wellsy_backend/src/main/java/com/kh/wellsy/chat/controller/ChatController.java
+++ wellsy_backend/src/main/java/com/kh/wellsy/chat/controller/ChatController.java
@@ -1,13 +1,31 @@
 package com.kh.wellsy.chat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.wellsy.chat.model.service.ChatService;
+import com.kh.wellsy.chat.model.vo.Chat;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/ai")
 public class ChatController {
 	
-	// - Message 객체: LLM과 사람이 나누는 대화 내용을 담는 객체 (부모타입)
-	// - UserMessage 객체: 사용자의 텍스트 메시지를 담는 객체 (자식타입)
-	// - AiMessage 객체: AI의 응답 메시지를 담는 객체 (자식타입)
+	@Autowired
+	private ChatService chatService;
+	
+	// 프론트에서 { message: "..." } 형태로 요청을 받아서
+	// { reply: "..." } 형태의 JSON으로 응답
+	public ResponseEntity<Chat> chat(@RequestBody Chat chat) {
+		
+		String reply = chatService.getAiReply(chat.getMessage());
+		
+		chat.setReply(reply);
+		
+		return ResponseEntity.ok(chat);
+	}
 }
