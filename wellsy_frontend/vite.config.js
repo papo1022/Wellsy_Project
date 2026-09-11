7a0657d3465bd9ad4ee4e1de913944e63f1bb79c
papo1022 <85226430+papo1022@users.noreply.github.com>
2026-09-11 10:37:03 +0900

    로컬

--- wellsy_frontend/vite.config.js
+++ wellsy_frontend/vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+
+  server: {
+    proxy: {
+      '/wellsy': {
+        target: 'http://localhost:8006',
+        changeOrigin: true,
+      }
+    }
+  }
 })
